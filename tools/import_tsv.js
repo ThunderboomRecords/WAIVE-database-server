98a186ef1c7e71571a9b239a52595abcf604c522
arran lyon <arran.lyon@hotmail.com>
2025-06-02 16:01:31 +0200

    Added import TSV tool for migrations

---
+++ tools/import_tsv.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { config } from 'dotenv';
+
+import { parse } from 'csv-parse';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const CONFIG_FILE = process.argv[2] || '.env';
+console.log("Loading config file: " + CONFIG_FILE);
+
+config({ path: CONFIG_FILE });
+
+let DATABASE_FILE = process.env.DATABASE_FILE || 'data.db';
+if (DATABASE_FILE[0] !== '/') {
+    DATABASE_FILE = path.join(__dirname, '..', DATABASE_FILE);
+}
+
+console.log(DATABASE_FILE);
+let dbPromise = open({
+    filename: DATABASE_FILE,
+    driver: sqlite3.Database
+});
+const db = await dbPromise;
+
+fs.createReadStream("filelist.tsv")
+    .pipe(parse({ delimiter: "\t", from_line: 2 }))
+    .on("data", function (data) {
+        // console.log(data);
+        db.run('INSERT OR REPLACE INTO Sources(id, archive, description, tags, filename, license) VALUES (?, ?, ?, ?, ?, ?)', [
+            data[0], data[5], data[1],  data[2], data[4], data[7]
+        ]);
+
+        db.run('INSERT OR IGNORE INTO Archives(name) VALUES (?)', [data[5]]);
+        // db.run('INSERT OR REPLACE INTO Tags(id, tag, embedX, embedY, counts) VALUES (?, ?, ?, ?, ?)', [
+        //     data[0], data[1], data[2], data[3], data[4]
+        // ]);
+    })
+    .on("end", async function () {
+        await db.close();
+        console.log("finished");
+    })
+    .on("error", function (error) {
+        console.log(error.message);
+    });
